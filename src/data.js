596074d5208aa858ea12b7bc8cc5f9888cbe5665
N LirajKhanna <lirajkhanna2002@gmail.com>
2021-11-24 00:48:41 +0530

    Final Songs Commited

--- src/data.js
+++ src/data.js
@@ -9,25 +9,15 @@ function chillHop() {
       active: false,
       color: ["#8462D3", "#AAD9F3"],
       cover:
-      "https://chillhop.com/wp-content/uploads/2021/09/6a9ef8ac00e168d3308fdf9e336874c03fab829d-1024x1024.jpg",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=23195",
-    },
-    {
-      name: "Daylight",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-      artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
-      color: ["#EF8EA9", "#ab417f"],
-      id: uuidv4(),
-      active: false,
+        "https://chillhop.com/wp-content/uploads/2021/09/6a9ef8ac00e168d3308fdf9e336874c03fab829d-1024x1024.jpg",
+      audio: "https://audio.jukehost.co.uk/Wy6j1mU4VXPzlW7xdorshkjfzVm46nM2",
     },
     {
       name: "Keep Going",
       cover:
-      "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
       artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+      audio: "https://audio.jukehost.co.uk/GCaEYXOIY6jcR6qidSohoPoQLEvBNQpZ",
       color: ["#CD607D", "#c94043"],
       id: uuidv4(),
       active: false,
@@ -37,7 +27,7 @@ function chillHop() {
       cover:
         "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
       artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+      audio: "https://audio.jukehost.co.uk/d7hGjVPQS1DO099cz3FGQPrYztJSFkyT",
       color: ["#205950", "#2ab3bf"],
       id: uuidv4(),
       active: true,
@@ -47,7 +37,7 @@ function chillHop() {
       cover:
         "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
       artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+      audio: "https://audio.jukehost.co.uk/XPD7gxhnykp8E7pVSVcJRe6uW5qULW1V",
       color: ["#EF8EA9", "#ab417f"],
       id: uuidv4(),
       active: false,
@@ -57,7 +47,7 @@ function chillHop() {
       cover:
         "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
       artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+      audio: "https://audio.jukehost.co.uk/6SdaouUGXe2x5F5GfSchYqicmHE2HZFa",
       color: ["#CD607D", "#c94043"],
       id: uuidv4(),
       active: false,
@@ -67,7 +57,7 @@ function chillHop() {
       cover:
         "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
       artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+      audio: "https://audio.jukehost.co.uk/kixLFmiMv3kNdd52x4m3YluDnGiavbwv",
       color: ["#205950", "#2ab3bf"],
       id: uuidv4(),
       active: false,
@@ -77,7 +67,7 @@ function chillHop() {
       cover:
         "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
       artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+      audio: "https://audio.jukehost.co.uk/KJLe6Nl9IQzO1mdTQA3lRQmiVEKCpb54",
       color: ["#EF8EA9", "#ab417f"],
       id: uuidv4(),
       active: false,
@@ -87,7 +77,7 @@ function chillHop() {
       cover:
         "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
       artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+      audio: "https://audio.jukehost.co.uk/FfxIB5c4fkJQsR7UfEtQopmlnEZs6jCj",
       color: ["#CD607D", "#c94043"],
       id: uuidv4(),
       active: false,
@@ -97,7 +87,7 @@ function chillHop() {
       cover:
         "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
       artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+      audio: "https://audio.jukehost.co.uk/NBLjSlbLTD3b0mpXbS28ZUYjO5JDaO3m",
       color: ["#205950", "#2ab3bf"],
       id: uuidv4(),
       active: false,
